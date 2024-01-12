9266c3b17bba78371005ec7196b7fe56f1b838e8
Cloud User <ubuntu@gpu-1.novalocal>
2024-01-12 16:25:43 +0900

    ver pretraining

--- utils/crwaling.py
+++ utils/crwaling.py
@@ -6,42 +6,14 @@ import time
 from tqdm.auto import tqdm
 import json
 
-mapping_alphabets = {'a': 167,
- 'b': 110,
- 'c': 227,
- 'd': 121,
- 'e': 94,
- 'f': 99,
- 'g': 46,
- 'h': 44,
- 'i': 60,
- 'j': 21,
- 'k': 9,
- 'l': 71,
- 'm': 100,
- 'n': 59,
- 'o': 55,
- 'p': 174,
- 'q': 18,
- 'r': 99,
- 's': 109,
- 't': 92,
- 'u': 32,
- 'v': 33,
- 'w': 36,
- 'x': 1,
- 'y': 3,
- 'z': 3}
-
 data_list = []
 
-URL = "https://thelawdictionary.org/letter/"
 
 cnt = 0
 
 for alphabet, num in mapping_alphabets.items():
 
-    URL = "https://thelawdictionary.org/letter/"
+    URL = "url_url_url"
     URL += alphabet
 
     for i in tqdm(range(1, num + 1)):
